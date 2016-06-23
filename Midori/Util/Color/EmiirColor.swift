7b6931fd0ab6921a1c73f307253154080d66a40c
hanwe <hanwe@msn.com>
2016-06-23 14:52:04 +0800

    Commit

---
+++ Midori/Util/Color/EmiirColor.swift
@@ -0,0 +1,17 @@
+//
+//  EmiirColor.swift
+//  Emiir
+//
+//  Created by hanwe on 2016/4/27.
+//  Copyright © 2016年 hanwe. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor
+{
+    class func barTintColor() -> UIColor
+    {
+        return UIColor(red: 18/255, green: 141/255 , blue: 229/255, alpha:1);
+    }
+}
